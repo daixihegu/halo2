c87f5d74e7f85c55ad3a42569aeeb0ab5ea5d678
Kris Nuttycombe <kris@nutty.land>
2026-05-31 09:11:45 -0600

    Fix clippy complaints.

--- halo2_gadgets/src/sinsemilla.rs
+++ halo2_gadgets/src/sinsemilla.rs
@@ -733,11 +733,7 @@ pub(crate) mod tests {
                     |(l, (left, right))| {
                         let merkle_crh = sinsemilla::HashDomain::from_Q((*Q).into());
                         let point = merkle_crh
-                            .hash_to_point(
-                                l.into_iter()
-                                    .chain(left.into_iter())
-                                    .chain(right.into_iter()),
-                            )
+                            .hash_to_point(l.into_iter().chain(left).chain(right))
                             .unwrap();
                         point.to_affine()
                     },
